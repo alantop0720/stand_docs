f777150a2f303f3e37018e55e34cd1151959ac3a
alantop <alantop@qq.com>
2023-04-10 13:37:05 +0800

    增加目录标准

---
+++ 建立标准部署目录.py
@@ -0,0 +1,10 @@
+import os
+
+os.mkdir("project")
+os.mkdir("project/interface")
+os.mkdir("project/envir")
+os.mkdir("project/db")
+os.mkdir("project/package")
+os.mkdir("project/maintain_docs")
+os.mkdir("project/tools")
+os.mkdir("project/code")
